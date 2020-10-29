e81444b337cefcbcef74c1cb8c603192fa02e522
AnnaBernad <73069123+AnnaBernad@users.noreply.github.com>
2020-10-29 19:43:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,300 @@
+/*let a = 'hello';
+console.log(a);
+alert(a);
+document.write(a);
+
+let b = 'owu';
+console.log(b);
+alert(b);
+document.write(b);
+
+let c = 'com';
+console.log(c);
+alert(c);
+document.write(c);
+
+let d = 'ua';
+console.log(d);
+alert(d);
+document.write(d);
+
+let e = 1;
+console.log(e);
+alert(e);
+document.write(e);
+
+let f = 10;
+console.log(f);
+alert(f);
+document.write(f);
+
+let g = -999;
+console.log(g);
+alert(g);
+document.write(g);
+
+let h = 123;
+console.log(h);
+alert(h);
+document.write(h);
+
+let i = 3.14;
+console.log(i);
+alert(i);
+document.write(i);
+
+let j = 2.7;
+console.log(j);
+alert(j);
+document.write(j);
+
+let k = 16;
+console.log(k);
+alert(k);
+document.write(k);
+
+let l = true;
+console.log(l);
+alert(l);
+document.write(l);
+
+let m = false;
+console.log(m);
+alert(m);
+document.write(m);
+
+a = 'bye';
+console.log(a);
+alert(a);
+document.write(a);
+
+b = 'uwo';
+console.log(b);
+alert(b);
+document.write(b);
+
+c = 'moc';
+console.log(c);
+alert(c);
+document.write(c);
+
+d = 'au';
+console.log(d);
+alert(d);
+document.write(d);
+
+e = -1;
+console.log(e);
+alert(e);
+document.write(e);
+
+f = -10;
+console.log(f);
+alert(f);
+document.write(f);
+
+g = 999;
+console.log(g);
+alert(g);
+document.write(g);
+
+h = -123;
+console.log(h);
+alert(h);
+document.write(h);
+
+i = -3.14;
+console.log(i);
+alert(i);
+document.write(i);
+
+j = -2.7;
+console.log(j);
+alert(j);
+document.write(j);
+
+k = -16;
+console.log(k);
+alert(k);
+document.write(k);
+
+l = false;
+console.log(l);
+alert(l);
+document.write(l);
+
+m = true;
+console.log(m);
+alert(m);
+document.write(m);
+
+const one = 1;
+console.log(one);
+alert(one);
+document.write(one);
+
+const two = -3.1;
+console.log(two);
+alert(two);
+document.write(two);
+
+const three = 2.8;
+console.log(j);
+alert(j);
+document.write(j);
+
+const onew = 'hi';
+console.log(onew);
+alert(onew);
+document.write(onew);
+
+const twow = 'he';
+console.log(twow);
+alert(twow);
+document.write(twow);
+
+const threew = 'ho';
+console.log(threew);
+alert(threew);
+document.write(threew);*/
+
+
+/*let lastname = prompt('Enter your lastname');
+ console.log(lastname);
+alert(lastname);
+ document.write(lastname);
+
+let name = prompt('Enter your name');
+console.log(name);
+alert(name);
+document.write(name);
+
+let surname = prompt('Enter your surname');
+console.log(surname);
+alert(surname);
+document.write(surname);
+
+let person = lastname + name + surname;
+
+document.write("<br>");
+
+lastname = 'Bernadovych';
+name = 'Tatyana';
+surname = 'Volodymyrivna';
+person = lastname + name + surname;
+console.log(person);
+document.write(person);
+alert(person);
+
+document.write("<br>");
+
+lastname = 'Bernadovych';
+name = 'Volodymyr';
+surname = 'Volodymyrovych';
+person = lastname + name + surname;
+console.log(person);
+document.write(person);
+alert(person);*/
+
+/*let numberone = + prompt('enter number');
+console.log(numberone+numberone);
+
+let numbertwo = + prompt('enter number');
+console.log(numbertwo);
+
+let numberthree = + prompt('enter number');
+console.log(numberthree);*/
+
+// let numberone = prompt('enter number 1');
+// numberone = parseInt(numberone);
+// console.log(numberone);
+
+// let numbertwo = prompt('enter number 2');
+// numbertwo = parseInt(numbertwo);
+// console.log(numbertwo);
+
+// let numberthree = prompt('enter number 3');
+// numberthree = parseInt(numberthree);
+// console.log(numberthree);
+
+// let numberfour = prompt('enter number 4');
+// numberfour = parseInt(numberfour);
+// console.log(numberfour);
+
+// let result = numberone + numbertwo + numberthree + numberfour;
+// console.log(result);
+
+// let numberone = prompt('enter number 1');
+// numberone = parseFloat(numberone);
+// numberone = Math.round(numberone);
+// console.log(numberone);
+
+// let numbertwo = prompt('enter number 2');
+// numbertwo = parseFloat(numbertwo);
+// numberone = Math.round(numberone);
+// console.log(numbertwo);
+
+// let numberthree = prompt('enter number 3');
+// numberthree = parseFloat(numberthree);
+// numberone = Math.round(numberone);
+// console.log(numberthree);
+
+// let result = numberone + numbertwo + numberthree;
+// console.log(result);
+
+// let numberone = +prompt('enter number 1');
+// numberone = Math.trunc(numberone);
+// console.log(numberone);
+
+// let numbertwo = +prompt('enter number 2');
+// numbertwo = Math.trunc(numbertwo);
+// console.log(numbertwo);
+
+// console.log(Math.pow(numberone,numbertwo));
+
+// let a = 100;
+// console.log(typeof a);
+
+// let b = '100';
+// console.log(typeof b);
+
+// let c = true;
+// console.log(typeof c);
+
+// let d = undefined;
+// console.log(typeof d);
+
+// let a = (5 < 6);
+// console.log(a);
+
+// let b = (5 > 6);
+// console.log(b);
+
+// let c = (5 == 6);
+// console.log(c);
+
+// let d = (5 >= 6);
+// console.log(d);
+
+// let e = (10 == 10);
+// console.log(e);
+
+// let f = (10 >= 10);
+// console.log(f);
+
+// let g = (10 !== 10);
+// console.log(g);
+
+// let h = (10 > 10);
+// console.log(h);
+
+// let i = (10 < 10);
+// console.log(i);
+
+// let ab = (123 === '123');
+// console.log(ab);
+
+// let ac = (123 >= '123');
+// console.log(ac);
+
+
