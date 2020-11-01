bd9c7c2d545019583e8e8cb4ee428242513f08d2
AnnaBernad <73069123+AnnaBernad@users.noreply.github.com>
2020-11-01 22:28:41 +0300

    Add files via upload

---
+++ task 2.js
@@ -0,0 +1,227 @@
+// let arrayone = [1, 5, 67, 980, 1.3];
+// let arraytwo = ['hello', 'hi', 'thanks', 'bye', 'idk'];
+// let arraythree = [5, 'hoy', true, 'none', 890];
+// console.log(arrayone);
+// console.log(arraytwo);
+// console.log(arraythree);
+
+
+// let arrayempty = [];
+// arrayempty[0] = 13;
+// arrayempty[1] = 'hi';
+// arrayempty[2] = 123;
+// arrayempty[3] = false;
+// arrayempty[4] = '134';
+// console.log(arrayempty);
+
+// let divs = ['Аа (лат. Aa) — род растений семейства Орхидные (подсемейство Orchidoideae, триба Cranichideae). Род включает 25[2] видов, произрастающих в Андах (Аргентина, Колумбия, Эквадор, Перу, Боливия, Венесуэла, Чили, Коста-Рика, Панама) на границе линии снегов возле горных ручьёв.', 'Первое научное описание видов рода было сделано в 1815 году Карлом Сигизмундом Кунтом', 'Род назван, согласно одной из версий, в честь Питера ван дер Аа (1659—1733), голландского издателя книг и карт', 'По другой версии род назван так с той целью, чтобы всегда быть первым в алфавитном порядке.', 'Название этого рода — одно из двух, наряду с Ио (Io B.Nord., 2003), самых коротких действительных названий родов сосудистых растений по данным сайта Germplasm Resources Information Network (GRIN).', 'Представители рода — травянистые растения с удлинёнными соцветиями', 'Цветки испускает острый запах, который привлекает мух.', 'Цветки испускает острый запах, который привлекает мух.', 'Aa achalensis Schltr. (1920) (Аргентина)', 'Aa argyrolepis Rchb.f. (1854) (Колумбия и Эквадор)',];
+// for (i = 0; i < 10; i++){
+//     document.write('<div>' + i + divs[i] + '</div>');  
+// }
+
+// let divs = ['Аа (лат. Aa) — род растений семейства Орхидные (подсемейство Orchidoideae, триба Cranichideae). Род включает 25[2] видов, произрастающих в Андах (Аргентина, Колумбия, Эквадор, Перу, Боливия, Венесуэла, Чили, Коста-Рика, Панама) на границе линии снегов возле горных ручьёв.', 'Первое научное описание видов рода было сделано в 1815 году Карлом Сигизмундом Кунтом', 'Род назван, согласно одной из версий, в честь Питера ван дер Аа (1659—1733), голландского издателя книг и карт', 'По другой версии род назван так с той целью, чтобы всегда быть первым в алфавитном порядке.', 'Название этого рода — одно из двух, наряду с Ио (Io B.Nord., 2003), самых коротких действительных названий родов сосудистых растений по данным сайта Germplasm Resources Information Network (GRIN).', 'Представители рода — травянистые растения с удлинёнными соцветиями', 'Цветки испускает острый запах, который привлекает мух.', 'Цветки испускает острый запах, который привлекает мух.', 'Aa achalensis Schltr. (1920) (Аргентина)', 'Aa argyrolepis Rchb.f. (1854) (Колумбия и Эквадор)',];
+// let i =0;
+// while(i < 10) {
+//     document.write('<div>' + '<h1>' + i + divs[i] + '</h1>' + '</div>');
+//     i++;
+// }
+
+// let numbers = [2, 5, 10, 3, 45, 567, 67, 254, 5688, 45];
+// for(i=0; i<10; i++){
+//     console.log(numbers[i]);
+// }
+
+// let string = ['2', '5', '10', '3', '45', '567', '67', '254', '5688', '45'];
+// for(i=0; i<10; i++){
+//     console.log(numbers[i]);
+// }
+
+// let numbers = [2, 'hello', true, 3, 'bye', 567, false, 254, '5688', 45];
+// for (let i = 0; i <10; i++){
+//     if (typeof(numbers[i]) == 'string'){
+//         console.log(numbers[i]);
+
+//     }
+// }
+// let numbers = [2, 'hello', true, 3, 'bye', 567, false, 254, '5688', 45];
+// for (let i = 0; i <10; i++){
+//     if (typeof(numbers[i]) == 'number'){
+//         console.log(numbers[i]);
+//     }
+// }
+// let numbers = [2, 'hello', true, 3, 'bye', 567, false, 254, '5688', 45];
+// for (let i = 0; i <10; i++){
+//     if (typeof(numbers[i]) == 'boolean'){
+//         console.log(numbers[i]);
+
+//     }
+// }
+
+// let emptyarr = [];
+// emptyarr[0] = 13;
+// emptyarr[1] = 'hi';
+// emptyarr[2] = 123;
+// emptyarr[3] = false;
+// emptyarr[4] = '134';
+// emptyarr[5] = 'hello';
+// emptyarr[6] = 15677;
+// emptyarr[7] = true;
+// emptyarr[8] = 'vasyl';
+// emptyarr[9] = false;
+// for (i = 0; i < 10; i++) {
+//     console.log(emptyarr[i]);
+// }
+
+// for ( i = 0; i < 10; i++){
+//     console.log(i);
+//     document.write(i);
+// }
+// for ( i = 0; i < 100; i++){
+//     console.log(i);
+//     document.write(i);
+// }
+// for ( i = 0; i < 100; i+=2){
+//     console.log(i);
+//     document.write(i);
+// }
+// for ( i = 1; i < 100; i+=2){
+//     console.log(i);
+//     document.write(i);
+// }
+// for (let i = 0; i <2; i++){
+//     for(let j = 0; j < 60; j++){
+//         console.log(i, j);
+//     }
+// }
+// for (let i = 0; i <3; i++){
+//     for(let j = 0; j < 20; j++){
+//         for( let k = 0; k < 60; k++){
+//             console.log(i,j,k);
+//         }
+       
+//     }
+// }
+// let arr = ['a', 'b','c'];
+// let a = '';
+// for(i = 0; i<arr.length; i++){
+//     document.write(arr[i]);
+//     a += arr[i];  
+      
+// }
+//  console.log(a);
+
+//  let arr = ['a', 'b','c'];
+//  let a = '';
+//  let i = 0;
+//  while (i <arr.length){
+//      document.write(arr[i]);
+//      a += arr[i];
+//      i++;
+//  }
+//  console.log(a);
+
+// let arr = ['a', 'b','c'];
+// for (let value of arr) {
+//     document.write(value);
+//   }
+
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+// while(i < numbers.length){
+//     console.log(numbers[i]);
+//     i++;
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i = 0; i < numbers.length; i++){
+//     if (numbers[i] % 2 == 0) {
+//     console.log(numbers[i]);
+//      }
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+// while(i < numbers.length){
+//     if(numbers[i] % 2 == 0){
+//     console.log(numbers[i]);
+//     } 
+//    i++; 
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i = 0; i < numbers.length; i++){
+//     if (numbers[i] % 2 !== 0) {
+//     console.log(numbers[i]);
+//      }
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = 0;
+// while(i < numbers.length){
+//     if(numbers[i] % 2 !== 0){
+//     console.log(numbers[i]);
+//     } 
+//    i++; 
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i = 0; i < numbers.length; i++){
+//     if (numbers[i] % 3 == 0) {
+//     numbers[i] = 'okten';
+    
+//      }
+//     console.log(numbers[i]);
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i = numbers.length - 1; i >= 0; i--){
+//     console.log(numbers[i]);
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = numbers.length - 1;
+// while(i >= 0){
+//     console.log(numbers[i]);
+//     i--;
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i = numbers.length - 1; i >= 0; i--){
+//     if (numbers[i] % 2 == 0) {
+//     console.log(numbers[i]);
+//      }
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = numbers.length -1;
+// while(i >= 0){
+//     if(numbers[i] % 2 == 0){
+//     console.log(numbers[i]);
+//     } 
+//    i--; 
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i = numbers.length -1; i >= 0 ; i--){
+//     if (numbers[i] % 2 !== 0) {
+//     console.log(numbers[i]);
+//      }
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// let i = numbers.length - 1;
+// while(i >= 0){
+//     if(numbers[i] % 2 !== 0){
+//     console.log(numbers[i]);
+//     } 
+//    i--; 
+// }
+// let numbers = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for(i =  numbers.length - 1; i >= 0; i--){
+//     if (numbers[i] % 3 == 0) {
+//     numbers[i] = 'okten';
+    
+//      }
+//     console.log(numbers[i]);
+// }
+// let emptyarr =[];
+// for(i=2; i <=50; i+=2){
+//     emptyarr.push(i);
+   
+// }
+// console.log(emptyarr);
+// let emptyarr =[];
+// for(i=1; i <=50; i+=2){
+//     emptyarr.push(i);
+   
+// }
+// console.log(emptyarr);
